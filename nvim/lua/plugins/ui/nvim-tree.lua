c0d960649ab2936f200f6beb26c123ec25984176
morningdip <morningdip@hotmail.com>
2024-02-06 17:09:39 +0800

    refine: dot files filter in nvim-tree

--- nvim/lua/plugins/ui/nvim-tree.lua
+++ nvim/lua/plugins/ui/nvim-tree.lua
@@ -1,12 +1,17 @@
 return {
   "nvim-tree/nvim-tree.lua",
-  enabled = true,
   dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
     disable_netrw = true,
     hijack_netrw = true,
     respect_buf_cwd = true,
     sync_root_with_cwd = true,
+    filters = {
+      dotfiles = false,
+      git_clean = false,
+      no_buffer = false,
+      custom = { ".DS_Store" }
+    },
   },
   keys = {
     { "<leader>e", "<cmd>NvimTreeToggle<CR>" },
